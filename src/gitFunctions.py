57608a4d134735cdf0c13749e53964f0c4230b81
Azalurg <91213389+Azalurg@users.noreply.github.com>
2021-12-29 13:34:00 +0100

    pull, push, ststus, .bat

--- src/gitFunctions.py
+++ src/gitFunctions.py
@@ -22,9 +22,35 @@ def pull_project(self):
         repo = git.Repo(self)
         remote = repo.remote()
         remote.pull()
-
     except Exception as e:
         success = False
         error = e
+    return success, error
 
+
+def show_status(self):
+    success, error = True, None
+    try:
+        repo = git.Repo(self)
+        if repo.index.diff(None) and repo.untracked_files:
+            print("--------------------------------------------------------")
+            print(self[:-1])
+            print(repo.git.status(), '\n')
+        else:
+            pass
+    except Exception as e:
+        success = False
+        error = e
+    return success, error
+
+
+def push_all(self):
+    success, error = True, None
+    try:
+        repo = git.Repo(self)
+        remote = repo.remote()
+        remote.push()
+    except Exception as e:
+        success = False
+        error = e
     return success, error
